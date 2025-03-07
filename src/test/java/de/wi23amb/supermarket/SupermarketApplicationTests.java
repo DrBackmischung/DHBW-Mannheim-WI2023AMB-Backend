d456814dac682d626d1655a90adc21bf406045ba
Mathis Neunzig <mathis.neunzig@gmail.com>
2025-03-07 09:54:17 +0100

    Add files via upload

---
+++ src/test/java/de/wi23amb/supermarket/SupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wi23amb.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
